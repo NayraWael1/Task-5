4f1d89365baa9861805521bb3cf9dbcd2a3b70ee
Nayra Wael <168375754+NayraWael1@users.noreply.github.com>
2024-07-26 23:19:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//1
+console.log(10 + 20 * 3  + 30 * 5  + 30 -250);
+
+
+//2-
+let item1price = 10;
+let item2price = 5;
+let item3price =  item2price * 2;
+let totalPriceIsBeforeDiscount  = item1price + item2price + item3price;
+console.log(totalPriceIsBeforeDiscount);
+let discount = 5 ;
+let finslPrice = totalPriceIsBeforeDiscount - discount ;
+console.log(finslPrice);
+
+
+//3
+let numOne  = 20;
+let numTwo  = 24;
+console.log( "" + numOne + numTwo);
+console.log(typeof ("" + numOne + numTwo))
+console.log(`${numOne}${numTwo}`);
+console.log(typeof (`${numOne}${numTwo}`));
+console.log( numOne + "\n" +numTwo);
+console.log(`${numOne} \n${numTwo}`);
+
+console.log(10**3)
+
+
